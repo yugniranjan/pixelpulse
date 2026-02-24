b67ddfe0435da0d3f448859171bc3837d2ab99d0
yogesh niranjan <yogeshnirajan786@gmail.com>
2026-02-24 16:36:40 +0530

    fix server issue

--- next.config.mjs
+++ next.config.mjs
@@ -34,6 +34,7 @@ const nextConfig = {
     ],
   },
   env: {
+    NEXT_PUBLIC_API_URL: "https://apis-351216.nn.r.appspot.com/api",
     NEXT_PUBLIC_BASE_URL: "https://www.pixelpulseplay.ca",
   },
 };
