9353f8b99274f4cf8e3efe5b80b43c99e0387678
vijadi005 <vijayant.verma@gmail.com>
2026-04-11 16:45:56 -0400

    Add files via upload

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import "./styles/home.css";
 import "./styles/pagenew.css";
 import "./styles/promotions.css";
+import "./styles/category.css";
 import Image from "next/image";
 import Link from "next/link";
 import { getDataByParentId } from "@/utils/customFunctions";
